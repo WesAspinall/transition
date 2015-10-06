71233766d49cd97e10bd6099ec4878c86580384b
Wesley Aspinall <wra427@gmail.com>
2015-10-06 19:22:31 -0400

    added comments

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function change(event){
-  var parent = $(event.currentTarget).parent();
-      parent.addClass('expand');
-      parent.siblings().removeClass('expand');
 
-  console.log("click");
+  var parent = $(event.currentTarget).parent();///this targets h1
+
+      parent.addClass('expand');///this gives h1 the class of .expand
+
+      parent.siblings().removeClass('expand');//for each  of the siblings, the expand class is removed 
+                                              //and we are left with the base state (32px height)
+
+  // console.log("click");
 };
 
 
-$('div').on('click','a', change);
+$('div').on('click','a', change);//how we get it to show up on the page
